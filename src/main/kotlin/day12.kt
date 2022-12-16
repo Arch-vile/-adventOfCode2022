5adb3c3a28999554d9d02f7a90a766fa5810be80
Arch-vile <mikko.ravimo@gmail.com>
2022-12-16 15:17:33 +0200

    day 12 part 2 done

--- src/main/kotlin/day12.kt
+++ src/main/kotlin/day12.kt
@@ -8,18 +8,45 @@ import aoc.utils.graphs.shortestPath
 import aoc.utils.readInput
 import aoc.utils.toList
 import java.awt.geom.GeneralPath
+import java.lang.Exception
 
 data class Square(var height: Char, var isVisited: Boolean)
 
 fun main() {
-    part1()
+    part2()
+}
+
+fun part2(): Int {
+    val map = Matrix(readInput("day12-input.txt")
+        .map { it.toList().map { Node(Square(it[0], false)) } })
+    val start = map.find { it.value.height == 'S' }!!
+    val end = map.find { it.value.height == 'E' }!!
+
+    start.value.value.height = 'a'
+    end.value.value.height = 'z'
+    buildGraph(map, start)
+
+    val anyShortest = map.findAll { it.value.value.height == 'a' }
+        .map {
+            try {
+                val foo = shortestPath(it.value, end.value)!!
+                foo
+            } catch (e: Exception) {
+                // TODO shortest path should not error
+                10000
+            }
+        }
+        .minOf { it }
+
+    println(anyShortest)
+    return 1;
 }
 
 fun part1(): Int {
     val map = Matrix(readInput("day12-input.txt")
         .map { it.toList().map { Node(Square(it[0], false)) } })
-    val start = map.find{ it.value.height == 'S'}!!
-    val end = map.find{ it.value.height == 'E'}!!
+    val start = map.find { it.value.height == 'S' }!!
+    val end = map.find { it.value.height == 'E' }!!
 
     start.value.value.height = 'a'
     end.value.value.height = 'z'
@@ -27,7 +54,7 @@ fun part1(): Int {
     val graph = buildGraph(map, start)
 
     val foo = shortestPath(start.value, end.value)
-   println(foo)
+    println(foo)
     return 1;
 }
 
@@ -43,12 +70,9 @@ fun buildGraph(map: Matrix<Node<Square>>, current: Entry<Node<Square>>): Entry<N
 
     accessible
         .filter { !it.value.value.isVisited }
-        .forEach { buildGraph(map,it ) }
+        .forEach { buildGraph(map, it) }
 
     return current
 }
 
 
-fun part2(): Int {
-    return 1;
-}
