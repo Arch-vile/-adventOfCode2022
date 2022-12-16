05e6244ec2e0a4e40232b50adcb04661e8530aab
Arch-vile <mikko.ravimo@gmail.com>
2022-12-16 15:17:31 +0200

    temp

--- src/main/kotlin/day6.kt
+++ src/main/kotlin/day6.kt
@@ -3,8 +3,11 @@ package day6
 import utils.readInput
 
 fun part1(): Int {
-    readInput("day6-input.txt")
-    return 1;
+    val marker = readInput("day6-input.txt")
+        .windowed(4,1)
+        .first { it.intersect(it).size == 4 }
+        .joinToString("")
+    return readInput("day6-input.txt")[0].indexOf(marker)
 }
 
 fun part2(): Int {
