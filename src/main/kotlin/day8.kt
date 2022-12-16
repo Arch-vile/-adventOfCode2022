2c345e75984fb87db750a4789a68cc03132b791b
Arch-vile <mikko.ravimo@gmail.com>
2022-12-16 15:17:31 +0200

    temp

--- src/main/kotlin/day8.kt
+++ src/main/kotlin/day8.kt
@@ -4,25 +4,30 @@ import aoc.utils.Matrix
 import aoc.utils.readInput
 import aoc.utils.toList
 
+data class Tree(val name: String, val hight: Int)
+
 fun part1(): Int {
     val input = readInput("day8practice-input.txt")
         .map { it.toList().map { it.toInt() } }
 
-    val inLines = countInLines(input)
+    val trees = input.mapIndexed { y, row -> row.mapIndexed { x, column -> Tree("$y$x", column)  }  }
+
+    val inLines = countInLines(trees)
     println(inLines)
 
-    val inRows = countInLines(Matrix(input).rotateCW().values())
+    val inRows = countInLines(Matrix(trees).rotateCW().values())
     println(inRows)
 
     val combined = inLines.union(inRows)
     println(combined)
 
     // 1121 not correct
+    // 1531 not correct
     return combined.size + input.size*2 + (input[0].size-2)*2;
 }
 
-private fun countInLines(input: List<List<Int>>): MutableSet<Pair<Int, Int>> {
-    val trees = mutableSetOf<Pair<Int, Int>>()
+private fun countInLines(input: List<List<Tree>>): MutableSet<Tree> {
+    val trees = mutableSetOf<Tree>()
 
     for (y in 1..input.size - 2) {
         for (x in 1..input[0].size - 2) {
@@ -31,8 +36,8 @@ private fun countInLines(input: List<List<Int>>): MutableSet<Pair<Int, Int>> {
             val before = currentRow.subList(0, x)
             val after = currentRow.subList(x + 1, currentRow.size)
 
-            if (before.maxOf { it } < input[y][x] || after.maxOf { it } < input[y][x])
-                trees.add(Pair(y, x))
+            if (before.maxOf { it.hight } < input[y][x].hight || after.maxOf { it.hight } < input[y][x].hight)
+                trees.add(input[y][x])
         }
     }
     return trees
