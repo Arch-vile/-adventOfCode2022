8f5924b00200bc3a7773dcc2c81d8c592e101a66
Arch-vile <mikko.ravimo@gmail.com>
2022-12-16 15:17:32 +0200

    refactor part 2

--- src/main/kotlin/day10.kt
+++ src/main/kotlin/day10.kt
@@ -3,92 +3,37 @@ package day10
 import aoc.utils.*
 
 fun part1(): Int {
+    val valueByCycle = valuesByCycle("day10-input.txt")
 
+    return (20..220).step(40)
+        .map { valueByCycle[it - 1] }
+        .sumOf { value -> value.first * value.second }
+}
+
+fun part2(): String {
+    val valuesByCycle = valuesByCycle("day10-input.txt")
+    var crt = ""
+    valuesByCycle.map { it.second }.forEachIndexed { i, value ->
+        if (closedRange(value - 1, value + 1).contains(i % 40)) {
+            crt += "#"
+        } else crt += "."
+    }
+    return crt.windowed(40, 40).joinToString("") { "$it\n" }
+}
+
+private fun valuesByCycle(file: String): List<Pair<Int, Int>> {
     var registerX = MutableInt(1)
 
     // Try with reduce also
-    val registerValues =
-        readInput("day10-input.txt")
-            .flatMap {
-                if (it.firstPart() == "noop") {
-                    listOf(registerX.value)
-                } else {
-                    listOf(
-                        registerX.value,
-                        registerX.plus(it.secondAsInt())
-                    )
-                }
-            }
+    val registerValues = readInput(file).flatMap {
+        if (it.firstPart() == "noop") {
+            listOf(registerX.value)
+        } else {
+            listOf(
+                registerX.value, registerX.plus(it.secondAsInt())
+            )
+        }
+    }
 
-    val valueByCycle =
-        listOf( Pair(1,1)) +
-        registerValues.mapIndexed { index, value -> Pair(index + 2, value) }
-
-    return listOf(
-        valueByCycle[20 - 1],
-        valueByCycle[60 - 1],
-        valueByCycle[100 - 1],
-        valueByCycle[140 - 1],
-        valueByCycle[180 - 1],
-        valueByCycle[220 - 1]
-    ).sumOf { value -> value.first * value.second }
-}
-
-
-fun part2(): String {
-
-    var cycle = 1
-    var register = 1
-    var executing = mutableListOf<String>()
-    var crtRow = mutableListOf<String>()
-
-    var crtResult = mutableListOf<String>()
-
-
-    val result = listOf(Pair(0, register)).plus(readInput("day10-input.txt")
-        .flatMap {
-
-            if (it.firstPart() == "noop") {
-                executing += "noop"
-            }
-
-            if (it.firstPart() == "addx") {
-                executing += "noop"
-                executing += it
-            }
-
-            val registerValues = executing.map {
-                val currentCycle = cycle
-                val currentRegister = register
-
-                if (closedRange(currentRegister - 1, currentRegister + 1).contains((currentCycle - 1) % 40)) {
-                    crtRow += "#"
-                } else {
-                    crtRow += "."
-                }
-                if (crtRow.size == 40) {
-                    crtResult += crtRow.joinToString("")
-                    crtRow.clear()
-                }
-
-                if (it.firstPart() == "addx") {
-                    register += it.secondAsInt()
-                }
-                cycle++
-                Pair(currentCycle, currentRegister)
-            }
-            executing.clear()
-
-            registerValues
-        })
-
-
-    return crtResult.joinToString("/")
-
-
-}
-
-
-fun strength(state: Pair<Int, Int>): Int {
-    return state.first * state.second
+    return listOf(Pair(1, 1)) + registerValues.mapIndexed { index, value -> Pair(index + 2, value) }
 }
