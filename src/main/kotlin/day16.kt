4f13b9183bda5accd9363eae159c1d4739dc4683
Arch-vile <mikko.ravimo@gmail.com>
2022-12-16 15:17:34 +0200

    day 16 graph created

---
+++ src/main/kotlin/day16.kt
@@ -0,0 +1,45 @@
+package day16
+
+import aoc.utils.findInts
+import aoc.utils.graphs.Node
+import aoc.utils.read
+import aoc.utils.readInput
+import aoc.utils.splitOn
+
+data class Valve(val name: String, val flow: Int)
+
+
+fun main() {
+    val valves = readInput("test.txt")
+        .map { it.split(" ").let { it[1] to it[4].findInts()[0] } }
+        .map { Node(Valve(it.first, it.second)) }
+
+   readInput("test.txt")
+        .map { it.replace("valve ", "valves ") }
+        .map {
+            it.split(" ").let {
+                val connected = it.splitOn { it == "valves" }[1].map { it.replace(",", "") }
+                it[1] to connected
+            }
+        }
+        .forEach { connection ->
+            val valve = valves.firstOrNull { it.value.name == connection.first  }!!
+            connection.second.forEach { target ->
+                val targetValve = valves.firstOrNull { it.value.name == target  }!!
+                valve.link(1, targetValve)
+            }
+        }
+
+    val start = valves.firstOrNull { it.value.name == "AA" }
+    println("done")
+
+}
+
+fun part1(): Int {
+    readInput("dayWIP-input.txt")
+    return 1;
+}
+
+fun part2(): Int {
+    return 1;
+}
