d554276a449297d8e5fc93dc02d53138b940eb0c
Arch-vile <mikko.ravimo@gmail.com>
2022-12-21 17:11:24 +0200

    answer for part 2 but it is wrong

--- src/main/kotlin/day21.kt
+++ src/main/kotlin/day21.kt
@@ -2,9 +2,10 @@ package day21
 
 import aoc.utils.findFirstInt
 import aoc.utils.readInput
+import java.math.BigInteger
 
 fun main() {
-    part1().let { println(it) }
+    part2().let { println(it) }
 }
 
 data class Monkey(
@@ -22,21 +23,70 @@ data class Monkey(
     }
 
     fun valued(): Long {
-        if(value != null)
+        if (value != null)
             return value!!
 
         return when (operation) {
-            '+' -> left!!.valued() + right!!.valued()
-            '-' -> left!!.valued() - right!!.valued()
-            '*' -> left!!.valued() * right!!.valued()
-            '/' -> left!!.valued() / right!!.valued()
+            '+' -> Math.addExact(left!!.valued(),  right!!.valued())
+            '-' -> Math.addExact(left!!.valued() , -1 * right!!.valued())
+            '*' -> Math.multiplyExact(left!!.valued(),  right!!.valued())
+            '/' -> {
+                val leftv = left!!.valued()
+                val rightV = right!!.valued()
+                Math.divideExact(leftv, rightV)
+            }
             else -> throw Error("unhandled $operation")
         }
-
     }
 }
 
 fun part1(): Long {
+    val monkeys = monkeys()
+    val root = monkeys["root"]!!
+    return root.valued()
+}
+
+fun part2(): Long {
+    val monkeys = monkeys()
+    val root = monkeys["root"]!!
+    val you = monkeys["humn"]!!
+
+    var increment = 1L
+    var test = increment
+    var lastUnder =  test
+
+//    you.value = 3887609741192
+    while (true) {
+        you.value = test
+        val left = root.left!!.valued()
+        val right = root.right!!.valued()
+
+        if (left == right) {
+            return you.value!!
+        }
+
+        // Left starts bigger than right
+        // Left seems to be decreasing as we increase test
+        if(left > right) {
+             lastUnder = test
+            increment += 2
+        }
+
+//        Went too far
+        if(left < right) {
+            increment = 1
+            test = lastUnder
+        }
+
+//        println("${test.toString().padStart(10)} ${(left-right).toString().padStart(20)}")
+        test += increment
+    }
+
+    // 3887609741192 too high
+    return 0
+}
+
+private fun monkeys(): Map<String, Monkey> {
     val monkeys = readInput("day21-input.txt")
         .map {
             val split = it.split(" ")
@@ -65,19 +115,11 @@ fun part1(): Long {
 
     // Link monkeys
     monkeys.values.forEach { monkey ->
-        if(monkey.leftPlaceHolder != null) {
+        if (monkey.leftPlaceHolder != null) {
             monkey.left = monkeys[monkey.leftPlaceHolder]
             monkey.right = monkeys[monkey.rightPlaceHolder]
         }
     }
-
-    val root = monkeys["root"]!!
-
-
-
-    return root.valued()
+    return monkeys
 }
 
-fun part2(): Int {
-    return 1;
-}
